ad044b17df9dcf4c3e9a6aab2dd977fd9f83d902
Pablo <pabloscaramutti@gmail.com>
2020-08-26 11:57:12 -0300

    Algunas configs

--- client-tesis/src/views/About.vue
+++ client-tesis/src/views/About.vue
@@ -2,6 +2,7 @@
   <div class="about">
     <h1>This is an about page</h1>
     <button type="button" @click="cerrarSesion">Loggout</button>
+    <button type="button" @click="iniciarSession">Login</button>
   </div>
 </template>
 
@@ -13,6 +14,11 @@ export default {
     cerrarSesion() {
       this.$store.dispatch(AccionesNombres.Logout);
       this.$router.push({ name: "Home" }).catch(err => {
+        return err;
+      });
+    },
+    iniciarSession() {
+      this.$router.push({ name: "Login" }).catch(err => {
         return err;
       });
     }
